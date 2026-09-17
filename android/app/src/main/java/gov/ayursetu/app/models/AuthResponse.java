29bca0532e5d3ccd6c5a860d59da2e75cb422aa5
Aryan Chauhan <aryan.chauhanarch7@gmail.com>
2026-09-17 17:04:28 +0530

    feat: add AuthResponse model for handling authentication responses

---
+++ android/app/src/main/java/gov/ayursetu/app/models/AuthResponse.java
@@ -0,0 +1,20 @@
+package gov.ayursetu.app.models;
+
+import com.google.gson.annotations.SerializedName;
+import java.io.Serializable;
+
+public class AuthResponse implements Serializable {
+    @SerializedName("token")
+    private String token;
+
+    @SerializedName("user")
+    private User user;
+
+    public AuthResponse() {}
+
+    public String getToken() { return token; }
+    public void setToken(String token) { this.token = token; }
+
+    public User getUser() { return user; }
+    public void setUser(User user) { this.user = user; }
+}
